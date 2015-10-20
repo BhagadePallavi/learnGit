2bbacaa260cae9f39b9c3ee83f92e2d0e1c58df4
yc03pb1 <pallavi.bhagade@macys.com>
2015-10-20 12:00:49 +0530

    Commiting 1st file

---
+++ test2.java
@@ -0,0 +1,24 @@
+
+public class test2
+{
+	
+	
+	public static void main(String s[])
+	{
+		System.out.println("My Name "+"Pallavi Bhagade");
+		
+	String str1="JP";
+	String str2 = new String ("JP");
+	int i=0;
+	i=i+=1;
+	
+	String []a ={"Pal","Pall","Pal"};
+	
+	for(String str:a)
+	{
+		System.out.println("value"+str);
+		System.out.println("value"+str.hashCode());
+	}
+
+    }
+}
